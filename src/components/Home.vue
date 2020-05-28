0dd2eab1a0414707333f94e40848b3b7cf7920a6
Alex <smsxander@gmail.com>
2020-05-29 00:49:56 +0200

    init weather-forecast test-project

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Hello</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  mounted() {
+    console.log("Component Mounted");
+  }
+};
+</script>
+
+<style scoped></style>
